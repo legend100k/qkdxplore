0b3a70bead5e7cfc45276b8b29ec20f70c8a09c4
Tejas N <tejasnaringre@gmail.com>
2025-09-24 23:45:26 +0530

    a:wq

--- src/components/PythonCodeEditor.tsx
+++ src/components/PythonCodeEditor.tsx
@@ -11,7 +11,7 @@ const PythonCodeEditor: React.FC = () => {
         rel="noopener noreferrer"
         className="inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
-        Open in Google Colab
+        Open in Google Colab:
       </a>
     </div>
   );
